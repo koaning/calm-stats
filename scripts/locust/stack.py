566f4bc9dcc50d46ed4e460ffd6f6593b0966410
vincent d warmerdam <vincentwarmerdam@gmail.com>
2022-09-04 09:59:38 +0000

    go

---
+++ scripts/locust/stack.py
@@ -0,0 +1,14 @@
+import typer 
+from pathlib import Path
+import pandas as pd
+
+
+def stack(new: Path, old: Path):
+    """
+    Stack new csv data on to some old one. 
+    """
+    pd.concat([pd.read_csv(new), pd.read_csv(old)]).to_csv(old)
+
+
+if __name__ == "__main__":
+    typer.run(stack)
