61e5801ac44e6623718bb225bda65301e61a0a72
vincent d warmerdam <vincentwarmerdam@gmail.com>
2022-09-04 12:10:28 +0000

    added

--- scripts/locust/stack.py
+++ scripts/locust/stack.py
@@ -1,14 +1,11 @@
-import typer 
-from pathlib import Path
 import pandas as pd
 
-
-def stack(new: Path, old: Path):
+def stack():
     """
     Stack new csv data on to some old one. 
     """
+    new = "locust_stats_history.csv"
+    old = "datasets/locust_stats_history.csv"
     pd.concat([pd.read_csv(new), pd.read_csv(old)]).to_csv(old)
 
-
-
-typer.run(stack)
+stack()
